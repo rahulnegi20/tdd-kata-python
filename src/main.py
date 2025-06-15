95b9c84624266c9c76632a88e6b4a4a82a7ff14e
unknown <rahulnegi2021@gmail.com>
2025-06-15 21:44:42 +0530

    negative test case passed

--- src/main.py
+++ src/main.py
@@ -7,11 +7,18 @@ class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
+        
         delimiter = ","
         if numbers.startswith("//"):
             delimiter_section, numbers = numbers.split("\n")
+            print(delimiter_section, numbers)
             delimiter = delimiter_section[2:]
         
         numbers = numbers.replace("\n", delimiter)
-        res = sum(int(num) for num in numbers.split(delimiter))
+        nums =  numbers.split(delimiter)
+        negative_numbers = [str(num) for num in nums if int(num) < 0]
+        if negative_numbers:
+            raise ValueError(f"negatives not allowed: {','.join(negative_numbers)}")
+        
+        res = sum(int(num) for num in nums)
         return res
